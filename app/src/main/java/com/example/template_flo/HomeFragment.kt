8491d401616c292efb62faf7259d4e0938c4a50f
iamgonnabe <wjdwlsdl321@naver.com>
2023-11-10 15:09:36 +0900

    .

---
+++ app/src/main/java/com/example/template_flo/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.template_flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.template_flo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homeAlbumImgIv1.setOnClickListener{
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
+        }
+        return binding.root
+    }
+}
